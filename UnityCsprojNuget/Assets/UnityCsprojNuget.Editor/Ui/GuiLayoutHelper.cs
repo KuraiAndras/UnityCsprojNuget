ffd6f206b3b878053c3666ffc039ff759dd84dd5
Kurai András <k.andras9642@gmail.com>
2020-06-22 22:59:48 +0200

    Thinner default line

--- UnityCsprojNuget/Assets/UnityCsprojNuget.Editor/Ui/GuiLayoutHelper.cs
+++ UnityCsprojNuget/Assets/UnityCsprojNuget.Editor/Ui/GuiLayoutHelper.cs
@@ -5,7 +5,7 @@ namespace UnityCsprojNuget.Editor.Ui
 {
     public static class GuiLayoutHelper
     {
-        public static void DrawUiLine(Color color, int thickness = 2, int padding = 10)
+        public static void DrawUiLine(Color color, int thickness = 1, int padding = 10)
         {
             var r = EditorGUILayout.GetControlRect(GUILayout.Height(padding + thickness));
             r.height = thickness;
